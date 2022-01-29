8c7d5675d30acc8b0589141218123582e582715e
gpassport <gppasaporte@student.apc.edu.ph>
2022-01-29 19:19:24 +0800

    Proxified API Endpoints and Created Proxy Config

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post('/api/' + 'signin', { //Removed AUTH_API changed to /api/ and create proxy.config.json to target the API
+      username,
+      password
+    }, httpOptions);
+  }
+
+//   register(username: string, email: string, password: string): Observable<any> {
+//     return this.http.post(AUTH_API + 'signup', {
+//       username,
+//       email,
+//       password
+//     }, httpOptions);
+//   }
+}
