d50ebeab5f823b4db89ae7a0e7f658df491928c1
jay1-53 <steven.daanton@gmail.com>
2022-02-25 16:58:04 +0800

    Renamed tabs1 to home tab

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,20 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
+import { Tab1PageRoutingModule } from './home-routing.module';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    ExploreContainerComponentModule,
+    Tab1PageRoutingModule
+  ],
+  declarations: [HomePage]
+})
+export class Tab1PageModule {}
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { PhotoService } from '../_services/photo.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss']
+})
+
+export class HomePage {
+
+  constructor(public photoService: PhotoService) { }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+}
